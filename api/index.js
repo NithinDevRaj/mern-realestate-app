6181cb9b6449246601e418c2c27137a2645b58ad
Nithin <nithinraj.dev@gmail.com>
2023-11-11 21:22:54 +0530

    bug fixing

--- api/index.js
+++ api/index.js
@@ -38,5 +38,9 @@ app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
+
 //configuring error middleware
 app.use(error);
+
+
+
